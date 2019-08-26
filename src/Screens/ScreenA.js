3e69c42a19f59904c8cb401ff3697cf374e3afd0
Isaac lem <isaac.lem@activate.sg>
2019-08-26 19:08:57 +0800

    Added new stories

--- src/Screens/ScreenA.js
+++ src/Screens/ScreenA.js
@@ -1,8 +1,1 @@
-import React from 'react';
-import { Text } from 'react-native';
-
-const ScreenA = () => (
-  <Text>ScreenA</Text>
-);
-
-export default ScreenA;
+export default from '../../storybook';
